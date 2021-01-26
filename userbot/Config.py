4d11235068fff0cc68821099c9b6902cf8cce9d2
Akhilsivakosana1 <76841158+Akhilsivakosana1@users.noreply.github.com>
2021-01-26 16:58:30 +0530

    Update Config.py

--- userbot/Config.py
+++ userbot/Config.py
@@ -1,2 +1,3 @@
-           "UPSTREAM_REPO", "https://github.com/Akhilsivakosana1/TCLUSERBOT"
+UPSTREAM_REPO = os.environ.get(
+            "UPSTREAM_REPO", "https://github.com/Akhilsivakosana1/TCLUSERBOT"
         )
